e81f61ba7cc648d9c7909e1b7f4d6b9002ca54f0
Gulshan Godsora <gulshangodsora008@gmail.com>
2025-12-22 12:31:59 +0530

    GUI: Login Font Update

--- gui/login_window.py
+++ gui/login_window.py
@@ -24,24 +24,27 @@ class LoginWindow:
         y = (sh // 2) - (height // 2)
         self.root.geometry(f"{width}x{height}+{x}+{y}")
 
+    title_font = ("Segoe UI", 20, "bold")
+    label_font = ("Segoe UI", 11, "bold")
+    entry_font = ("Segoe UI", 11, "bold")
+    button_font = ("Segoe UI", 11, "bold")
+
     def _create_widgets(self):
-        tk.Label(self.root, text="Admin Login", font=("Arial", 18, "bold")).pack(
-            pady=20
-        )
+        tk.Label(self.root, text="Admin Login", font=self.title_font).pack(pady=20)
 
         form_frame = tk.Frame(self.root)
         form_frame.pack(pady=10)
 
         # Username
-        tk.Label(form_frame, text="Username:", font=("Arial", 11)).grid(
+        tk.Label(form_frame, text="Username:", font=self.label_font).grid(
             row=0, column=0, padx=10, pady=10, sticky="e"
         )
 
-        self.username_entry = tk.Entry(form_frame, width=25)
+        self.username_entry = tk.Entry(form_frame, width=25, font=self.entry_font)
         self.username_entry.grid(row=0, column=1, pady=10)
 
         # Password
-        tk.Label(form_frame, text="Password:", font=("Arial", 11)).grid(
+        tk.Label(form_frame, text="Password:", font=self.label_font).grid(
             row=1, column=0, padx=10, pady=10, sticky="e"
         )
 
@@ -53,7 +56,7 @@ class LoginWindow:
             self.root,
             text="Login",
             width=15,
-            font=("Arial", 11, "bold"),
+            font=self.button_font,
             command=self.login,
         ).pack(pady=20)
 
